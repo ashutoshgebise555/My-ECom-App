28cc45f3a792eca776808366b5545c44dfca6c19
ashutoshgebise555 <ashutoshgebise555@gmail.com>
2025-05-30 00:41:16 +0530

    Updated app/page.tsx-description

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Footer from 'components/layout/footer';
 
 export const metadata = {
   description:
-    'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+    'High-performance ecommerce store built with Next.js, Vercel, and Shopify. Built with a great purpose and motivation. Please Support us.',
   openGraph: {
     type: 'website'
   }
